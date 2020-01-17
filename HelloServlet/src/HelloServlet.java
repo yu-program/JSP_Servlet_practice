1b9173fd45021a92212028a02eae3c0627800c03
yu-program <12ym.nkmr.11ym@gmail.com>
2020-01-17 17:51:19 +0900

    1月17日-3

---
+++ HelloServlet/src/HelloServlet.java
@@ -0,0 +1,38 @@
+//Servletはjavaの中にhtmlを記述していくイメージ
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<BODY>");
+		out.println("<h3>Hello Servlet!</h3>");
+		out.println("</BODY>");
+		out.println("</HTML>");
+	}
+}
